0249d48c8e52f18cc7014bc35ad90c03d422cf2b
mdsamir8863 <mentoonsdeveloper.ansari@gmail.com>
2024-02-28 15:52:53 +0530

    alfa aviation

---
+++ js/modal.js
@@ -0,0 +1,33 @@
+const modal = document.getElementById("myModal");
+
+function showModalAfterDelay() {
+  setTimeout(function () {
+    modal.style.display = "block";
+    removeBeforeUnloadListener(); // Remove beforeunload event listener when modal is displayed
+  }, 3000);
+}
+
+// Call the function when the page loads
+// window.addEventListener("load", showModalAfterDelay);
+
+// Event listener for beforeunload event
+function beforeUnloadHandler(e) {
+  // Cancel the event
+  e.preventDefault();
+  // Chrome requires returnValue to be set
+  e.returnValue = "";
+}
+
+function removeBeforeUnloadListener() {
+  window.removeEventListener("beforeunload", beforeUnloadHandler);
+}
+
+// Handle button click
+const button = document.getElementById("confirmButton");
+button.onclick = function () {
+  window.location.href = "../courses.html"; // Redirect to courses.html
+};
+
+function closeModal() {
+  modal.style.display = "none";
+}
